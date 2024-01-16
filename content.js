89b65121900909b3c40a2b5ecf5be3da6bc815e7
liran <liranmeir@gmail.com>
2024-01-16 22:05:05 +0200

    initialize project

---
+++ content.js
@@ -0,0 +1,1 @@
+console.log('Content script for My Extension is running!');
---
+++ popup.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Code here will run after the popup has loaded.
+  });
